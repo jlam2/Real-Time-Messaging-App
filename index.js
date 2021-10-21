41d3f6106695e15fe94239c97ca32d1badc06660
Jason Lam <32461232+jlam2@users.noreply.github.com>
2021-10-21 01:10:04 +0000

    Added location sharing and acknowledgements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http')
 const socketIO = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express();
 const server = http.createServer(app)
@@ -14,10 +15,23 @@ app.use(express.static(__dirname + '/public'))
 io.on('connection', (socket) => {
     console.log('New web socket connection')
     socket.emit('message', 'Welcome to the chat app!')
+    socket.broadcast.emit('message', 'A new user has joined')
 
-    socket.on('sendMessage', (msg) => {
+    socket.on('sendMessage', (msg, cb) => {
+        const filter = new Filter()
+        if(filter.isProfane(msg)) return cb("No profanity in messages please")
+
         io.emit('message', msg)
-        console.log(msg)
+        cb()
+    })
+
+    socket.on('sendLocation', (coords, cb) => {
+        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        cb()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
     })
 })
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,26 @@
 const socket = io()
+
 socket.on('message', (msg) => {
     document.querySelector("#output").innerHTML = msg
 })
 
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
+    socket.emit('sendMessage', e.target.elements.message.value, (error) => {
+        if(error){return console.log(error)}
+        console.log('Message delivered')
+    })
+})
 
-    socket.emit('sendMessage', e.target.elements.message.value)
+document.querySelector('#sendLocation').addEventListener('click', () => {
+    if(!navigator.geolocation) return alert('Geolocation not supported by browser')
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+        let long = pos.coords.latitude
+        let lat = pos.coords.longitude
+        socket.emit('sendLocation', {latitude: long ,longitude: lat}, () => {
+            console.log("Location Shared")
+        })
+    })
 })
 
