aaf353bff94ee76677ee4c43361baa9828b9c76f
Jason Lam <32461232+jlam2@users.noreply.github.com>
2021-10-22 21:46:44 +0000

    Added mustace templates to display messages dynamically

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,32 @@
 const socket = io()
 
+//elements
+const messageForm = document.querySelector('form')
+const messageFormInput = messageForm.querySelector('input')
+const messageFormButton = messageForm.querySelector('button')
+const sendLocationButton = document.querySelector('#sendLocation')
+const messages = document.querySelector('#messages')
+
+//templates
+const msgTemplate = document.querySelector('#msgTemplate').innerHTML
+
 socket.on('message', (msg) => {
-    document.querySelector("#output").innerHTML = msg
+    console.log(msg)
+    
+    const html = Mustache.render(msgTemplate, {message: msg})
+    messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('form').addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    messageFormButton.setAttribute('disabled', 'disabled')
+
     socket.emit('sendMessage', e.target.elements.message.value, (error) => {
+        messageFormButton.removeAttribute('disabled')
+        messageFormInput.value = ''
+        messageFormInput.focus()
+        
         if(error){return console.log(error)}
         console.log('Message delivered')
     })
@@ -15,12 +35,16 @@ document.querySelector('form').addEventListener('submit', (e) => {
 document.querySelector('#sendLocation').addEventListener('click', () => {
     if(!navigator.geolocation) return alert('Geolocation not supported by browser')
 
+    sendLocationButton.setAttribute('disabled', 'disabled')
+
     navigator.geolocation.getCurrentPosition((pos) => {
-        let long = pos.coords.latitude
-        let lat = pos.coords.longitude
-        socket.emit('sendLocation', {latitude: long ,longitude: lat}, () => {
-            console.log("Location Shared")
-        })
+        socket.emit('sendLocation', 
+            {latitude: pos.coords.longitude ,longitude: pos.coords.latitude}, 
+            () => {
+                sendLocationButton.removeAttribute('disabled')
+                console.log("Location Shared")
+            }
+        )
     })
 })
 
